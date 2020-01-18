1eafebf964202f5f82262ff7f9ed0ba4214a0e84
bilalmodern <bilalyet432@gmail.com>
2020-01-18 16:39:55 +0300

    2 files modified

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+
 const btnPlay = document.getElementById('play');
 btnPlay.onclick = function(){
 
-    
+  const soundFile = document.getElementById('high_a_sharp');
+  soundFile.play()
   
   
   };
