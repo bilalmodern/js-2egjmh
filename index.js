7f4a05337f91a7a644809635f8de8ca5578b4ee4
bilalmodern <bilalyet432@gmail.com>
2020-01-18 20:33:07 +0300

    3 files modified

--- index.js
+++ index.js
@@ -11,5 +11,22 @@ document.getElementById('buttonGreen').onclick = function(){
 document.getElementById('buttonRed').onclick = function(){
   document.getElementById('ses_red').play()
   };
+document.getElementById('buttonYellow').onclick = function(){
+  document.getElementById('ses_yellow').play()
+  };
 
-
+document.getElementById('buttonBlue').onclick = function(){
+  document.getElementById('ses_Blue').play()
+  };
+document.getElementById('buttonAkua').onclick = function(){
+  document.getElementById('ses_Akua').play()
+  };
+  document.getElementById('buttonPurple').onclick = function(){
+  document.getElementById('ses_Purple').play()
+  };
+  document.getElementById('buttonPink').onclick = function(){
+  document.getElementById('ses_Pink').play()
+  };
+  document.getElementById('buttonakuayesil').onclick = function(){
+  document.getElementById('ses_akuayesil').play()
+  };
