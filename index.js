9e3324f1658e3d53ffc7626ad82595a14e6903b4
bilalmodern <bilalyet432@gmail.com>
2020-01-18 20:07:54 +0300

    3 files modified

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascridddpt code!
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 
-const btnPlay = document.getElementById('play');
-btnPlay.onclick = function(){
 
-  const soundFile = document.getElementById('high_a_sharp');
-  soundFile.play()
-  
-  
+document.getElementById('buttonGreen').onclick = function(){
+  document.getElementById('ses_green').play()
   };
 
 
