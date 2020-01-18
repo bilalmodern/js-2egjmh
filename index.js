7d89fb8b4935ce53753a15de2547078e5edf60b4
bilalmodern <bilalyet432@gmail.com>
2020-01-18 16:32:03 +0300

    2 files modified

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 import './style.css';
 
 // Write Javascridddpt code!
+
+// Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const btnPlay = document.getElementById('play');
+btnPlay.onclick = function(){
+
+    
+  
+  
+  };
+
+
